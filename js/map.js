c9db8ed5cc034ada15b09cfda1e28d0f6ec2bc11
Jan Reimers <me@jan.rs>
2015-03-28 22:55:48 +0100

    added popups to the markers

--- js/map.js
+++ js/map.js
@@ -9,21 +9,20 @@ iconUrl: 'img/iconmonstr-location-icon-32.png',
 
 iconSize:     [32, 32],
 iconAnchor:   [32, 32],
-popupAnchor:  [0, 0]
+popupAnchor:  [-16, -35]
 });
 
 // locations of tha gitches
-var marker = L.marker([51.48138, 12.30518], {icon: monstrIcon}).addTo(map); // germany
-var marker = L.marker([46.544, 4.131], {icon: monstrIcon}).addTo(map); // france
-var marker = L.marker([47.98992, 17.17188], {icon: monstrIcon}).addTo(map); // austria
-var marker = L.marker([52.80276, 0.93359], {icon: monstrIcon}).addTo(map); // uk
-var marker = L.marker([23.40276, 78.92578], {icon: monstrIcon}).addTo(map); // india
-var marker = L.marker([4.03962, -70.82812], {icon: monstrIcon}).addTo(map); // colombia
-var marker = L.marker([42.09822, 45.59375], {icon: monstrIcon}).addTo(map); // georgia
-var marker = L.marker([50.6808,5.74609], {icon: monstrIcon}).addTo(map); // belgium
-var marker = L.marker([36.73888, -118.8828], {icon: monstrIcon}).addTo(map); // us, california
-var marker = L.marker([40.31304, -82.88086], {icon: monstrIcon}).addTo(map); // us, ohio
-
+var marker = L.marker([51.48138, 12.30518], {icon: monstrIcon}).bindPopup('Germany').addTo(map);
+var marker = L.marker([46.544, 4.131], {icon: monstrIcon}).bindPopup('France').addTo(map);
+var marker = L.marker([47.98992, 17.17188], {icon: monstrIcon}).bindPopup('Austria').addTo(map);
+var marker = L.marker([52.80276, 0.93359], {icon: monstrIcon}).bindPopup('UK').addTo(map);
+var marker = L.marker([23.40276, 78.92578], {icon: monstrIcon}).bindPopup('India').addTo(map);
+var marker = L.marker([4.03962, -70.82812], {icon: monstrIcon}).bindPopup('Colombia').addTo(map);
+var marker = L.marker([42.09822, 45.59375], {icon: monstrIcon}).bindPopup('Georgia').addTo(map);
+var marker = L.marker([50.6808,5.74609], {icon: monstrIcon}).bindPopup('Belgium').addTo(map);
+var marker = L.marker([36.73888, -118.8828], {icon: monstrIcon}).bindPopup('USA, California').addTo(map);
+var marker = L.marker([40.31304, -82.88086], {icon: monstrIcon}).bindPopup('USA, Ohio').addTo(map);
 
 // helper to find LonLat by click
 //function onMapClick(e) {
