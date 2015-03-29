2ddafb4c47ede399096122b85ecb5861e2213069
Felix De Montis <felix@demont.is>
2015-03-29 16:32:40 +0200

    coordinates of gitches get loaded from members file from main repo

--- js/map.js
+++ js/map.js
@@ -12,101 +12,16 @@ iconAnchor:   [16, 32],
 popupAnchor:  [0, -35]
 });
 
-// locations of tha gitches
-var gitches = [
-  {
-    "name": "Jan",
-    "github": "reimersjan",
-    "latlon": [53.553407, 9.992196]
-  },
-  {
-    "name": "Tim",
-    "github": "timpietrusky",
-    "latlon": [50.1109221, 8.6821267]
-  },
-  {
-    "name": "Kevin",
-    "github": "kevingimbel",
-    "latlon": [50.0123996, 8.4217125]
-  },
-  {
-    "name": "Max",
-    "github": "myxotod",
-    "latlon": [49.9928617, 8.2472526]
-  },
-  {
-    "name": "Luky",
-    "github": "LukyVj",
-    "latlon": [48.8603445, 2.30573760]
-  },
-  {
-    "name": "Felix",
-    "github": "dervondenbergen",
-    "latlon": [48.2081743, 16.3738189]
-  },
-  {
-    "name": "Kanu",
-    "github": "krman009",
-    "latlon": [22.3038945, 70.8021599]
-  },
-  {
-    "name": "Hugo",
-    "github": "DarbyBrown",
-    "latlon": [50.82253, -0.137163]
-  },
-  {
-    "name": "Scott",
-    "github": "scottnix",
-    "latlon": [32.715738, -117.1610838]
-  },
-  {
-    "name": "Nils",
-    "github": "schoenwaldnils",
-    "latlon": [53.553407, 9.992196]
-  },
-  {
-    "name": "Olly",
-    "github": "0x04",
-    "latlon": [47.8966802, 10.4151477]
-  },
-  {
-    "name": "Nick",
-    "github": "shvelo",
-    "latlon": [41.716667, 44.783333]
-  },
-  {
-    "name": "Ty",
-    "github": "tystrong",
-    "latlon": [39.440336, -84.3621634]
-  },
-  {
-    "name": "José",
-    "github": "jofpin",
-    "latlon": [4.598056, -74.075833]
-  },
-  {
-    "name": "Leeroyd",
-    "github": "leeroyd",
-    "latlon": [48.856614, 2.3522219]
-  },
-  {
-    "name": "Tim",
-    "github": "timbog80",
-    "latlon": [45.815115, -122.7426009]
-  },
-  {
-    "name": "Clément",
-    "github": "clementgalidie",
-    "latlon": [48.861417, 2.536463]
-  },
-  {
-    "name": "Haroen",
-    "github": "haroenv",
-    "latlon": [51.21996787685509, 3.2211304939333743]
-  }
-]
+// get locations of tha gitches via ajax
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '/members.json');
+xhr.send();
 
 var markers = {};
+
+xhr.onload = function() {
+  
+var gitches = JSON.parse( xhr.responseText ).gitches;
 
 for (var i = 0; i < gitches.length; i++) {
 
@@ -129,6 +44,8 @@ for (var i = 0; i < gitches.length; i++) {
 
   }
 
+}
+  
 }
 
 // helper to find LonLat by click
