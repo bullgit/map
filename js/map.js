68d1bc59df7bd888f8235112ea747c55ef93d842
Jan Reimers <me@jan.rs>
2015-03-29 01:31:46 +0100

    fixed marker zoom behavior

--- js/map.js
+++ js/map.js
@@ -8,18 +8,18 @@ var monstrIcon = L.icon({
 iconUrl: 'img/iconmonstr-location-icon-32.png',
 
 iconSize:     [32, 32],
-iconAnchor:   [32, 32],
+iconAnchor:   [16, 32],
 popupAnchor:  [-16, -35]
 });
 
 // locations of tha gitches
-var marker = L.marker([51.48138, 12.30518], {icon: monstrIcon}).bindPopup('Germany').addTo(map);
-var marker = L.marker([46.544, 4.131], {icon: monstrIcon}).bindPopup('France').addTo(map);
-var marker = L.marker([47.98992, 17.17188], {icon: monstrIcon}).bindPopup('Austria').addTo(map);
-var marker = L.marker([52.80276, 0.93359], {icon: monstrIcon}).bindPopup('UK').addTo(map);
+var marker = L.marker([51.48138, 10.30518], {icon: monstrIcon}).bindPopup('Germany').addTo(map);
+var marker = L.marker([47.544, 3.131], {icon: monstrIcon}).bindPopup('France').addTo(map);
+var marker = L.marker([47.98992, 16.17188], {icon: monstrIcon}).bindPopup('Austria').addTo(map);
+var marker = L.marker([52.80276, -1.93359], {icon: monstrIcon}).bindPopup('UK').addTo(map);
 var marker = L.marker([23.40276, 78.92578], {icon: monstrIcon}).bindPopup('India').addTo(map);
 var marker = L.marker([4.03962, -70.82812], {icon: monstrIcon}).bindPopup('Colombia').addTo(map);
-var marker = L.marker([42.09822, 45.59375], {icon: monstrIcon}).bindPopup('Georgia').addTo(map);
+var marker = L.marker([42.09822, 43.59375], {icon: monstrIcon}).bindPopup('Georgia').addTo(map);
 var marker = L.marker([51.213162, 3.222145], {icon: monstrIcon}).bindPopup('Belgium').addTo(map);
 var marker = L.marker([36.73888, -118.8828], {icon: monstrIcon}).bindPopup('USA, California').addTo(map);
 var marker = L.marker([40.31304, -82.88086], {icon: monstrIcon}).bindPopup('USA, Ohio').addTo(map);
