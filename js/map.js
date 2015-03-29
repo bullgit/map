8a153cb11347fac15f25a92a86ab4d4d3a8b8af6
Haroen Viaene <fingebimus@me.com>
2015-03-29 01:08:48 +0100

    I don't live in nowhere 😏

--- js/map.js
+++ js/map.js
@@ -20,7 +20,7 @@ var marker = L.marker([52.80276, 0.93359], {icon: monstrIcon}).bindPopup('UK').a
 var marker = L.marker([23.40276, 78.92578], {icon: monstrIcon}).bindPopup('India').addTo(map);
 var marker = L.marker([4.03962, -70.82812], {icon: monstrIcon}).bindPopup('Colombia').addTo(map);
 var marker = L.marker([42.09822, 45.59375], {icon: monstrIcon}).bindPopup('Georgia').addTo(map);
-var marker = L.marker([50.6808,5.74609], {icon: monstrIcon}).bindPopup('Belgium').addTo(map);
+var marker = L.marker([51.213162, 3.222145], {icon: monstrIcon}).bindPopup('Belgium').addTo(map);
 var marker = L.marker([36.73888, -118.8828], {icon: monstrIcon}).bindPopup('USA, California').addTo(map);
 var marker = L.marker([40.31304, -82.88086], {icon: monstrIcon}).bindPopup('USA, Ohio').addTo(map);
 
